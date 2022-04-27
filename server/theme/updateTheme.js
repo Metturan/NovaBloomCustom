003c8463baaa74caa18d9915f74c332747a2f724
metturan <metturanketh@gmail.com>
2022-04-27 12:36:01 -0400

    a

--- server/theme/updateTheme.js
+++ server/theme/updateTheme.js
@@ -7,7 +7,7 @@ import path from 'path'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 
