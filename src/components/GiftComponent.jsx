492fda5e0221c66c57e4d8376682d329cbdcbd5a
metturan <metturanketh@gmail.com>
2022-04-28 15:58:51 -0400

    a

--- src/components/GiftComponent.jsx
+++ src/components/GiftComponent.jsx
@@ -59,6 +59,8 @@ function GiftComponent (props) {
   if (loading) return <div>Loading...</div>
   if (error) return <div>{error.message}</div>
   console.log('no reaload')
+
+  console.log(data.nodes)
   if (data.nodes[0] === null) {
     // window.location.reload();
     return <div></div>
