b4b79744e61a8c700c8e0efef68f47ac7be4ef1e
metturan <metturanketh@gmail.com>
2022-05-17 09:20:03 -0400

    a

--- server/index.js
+++ server/index.js
@@ -123,6 +123,7 @@ export async function createServer(
             
             // collection upsell update
             if (collectionUpdatedId == upsellId) {
+              console.log('checking match')
               const session = await Shopify.Utils.loadCurrentSession(req,res);
               const client = new Shopify.Clients.Rest(
                 session.shop,
